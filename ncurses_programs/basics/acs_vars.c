96a6f77f123aba5d0a9b8f666a8f022e3d4753ce
nasciiboy <nasciiboy@localhost.localnascciboy>
2015-12-24 18:42:23 -0600

    ------------------------------------------------------- NCURSES-Programming-HOWTO.es - headline 13.3-16.1 + original ada files -------------------------------------------------------    modified:   NCURSES-Programming-HOWTO-es.org    modified:   NCURSES-Programming-HOWTO.org    modified:   NCURSES-Programming-HOWTO.html    new file:   index.org    modified:   hackguide.org    new file:   Ada95.org    new file:   ada/    modified:   ncurses_programs/basics/acs_vars.c ------------------------------------------------------- Sonic-Boom -------------------------------------------------------    Steve Vai  : The 7th Song    Portishead : Third    NGE        : THE BIRTHDAY OF Rei AYANAMI               : REFRAIN               : The Day of Second Impact               : VOX               : A.T.EVA01 Reference CD               : EVANGELION 1.0 YOU ARE (NOT) ALONE OST -------------------------------------------------------

--- ncurses_programs/basics/acs_vars.c
+++ ncurses_programs/basics/acs_vars.c
@@ -1,44 +1,23 @@
 #include <ncurses.h>
+#include <stdlib.h>
 
 int main()
 {
-        initscr();
+  initscr();                      /* Start curses mode                   */
+  printw("Hello World !!!\n");    /* Print Hello World                   */
+  refresh();                      /* Print it on to the real screen      */
+  getch();                        /* Wait                                */
+  def_prog_mode();                /* Save the tty modes                  */
+  endwin();                       /* End curses mode temporarily         */
+  system("/bin/sh");              /* Do whatever you like in cooked mode */
+  reset_prog_mode();              /* Return to the previous tty mode     */
+  /* stored by def_prog_mode()           */
+  refresh();                      /* Do refresh() to restore the         */
+  /* Screen contents                     */
+  printw("Another String\n");     /* Back to curses use the full         */
+  refresh();                      /* capabilities of curses              */
+  getch();                        /* Wait                                */
+  endwin();                       /* End curses mode                     */
 
-        printw("Upper left corner           "); addch(ACS_ULCORNER); printw("\n"); 
-        printw("Lower left corner           "); addch(ACS_LLCORNER); printw("\n");
-        printw("Lower right corner          "); addch(ACS_LRCORNER); printw("\n");
-        printw("Tee pointing right          "); addch(ACS_LTEE); printw("\n");
-        printw("Tee pointing left           "); addch(ACS_RTEE); printw("\n");
-        printw("Tee pointing up             "); addch(ACS_BTEE); printw("\n");
-        printw("Tee pointing down           "); addch(ACS_TTEE); printw("\n");
-        printw("Horizontal line             "); addch(ACS_HLINE); printw("\n");
-        printw("Vertical line               "); addch(ACS_VLINE); printw("\n");
-        printw("Large Plus or cross over    "); addch(ACS_PLUS); printw("\n");
-        printw("Scan Line 1                 "); addch(ACS_S1); printw("\n");
-        printw("Scan Line 3                 "); addch(ACS_S3); printw("\n");
-        printw("Scan Line 7                 "); addch(ACS_S7); printw("\n");
-        printw("Scan Line 9                 "); addch(ACS_S9); printw("\n");
-        printw("Diamond                     "); addch(ACS_DIAMOND); printw("\n");
-        printw("Checker board (stipple)     "); addch(ACS_CKBOARD); printw("\n");
-        printw("Degree Symbol               "); addch(ACS_DEGREE); printw("\n");
-        printw("Plus/Minus Symbol           "); addch(ACS_PLMINUS); printw("\n");
-        printw("Bullet                      "); addch(ACS_BULLET); printw("\n");
-        printw("Arrow Pointing Left         "); addch(ACS_LARROW); printw("\n");
-        printw("Arrow Pointing Right        "); addch(ACS_RARROW); printw("\n");
-        printw("Arrow Pointing Down         "); addch(ACS_DARROW); printw("\n");
-        printw("Arrow Pointing Up           "); addch(ACS_UARROW); printw("\n");
-        printw("Board of squares            "); addch(ACS_BOARD); printw("\n");
-        printw("Lantern Symbol              "); addch(ACS_LANTERN); printw("\n");
-        printw("Solid Square Block          "); addch(ACS_BLOCK); printw("\n");
-        printw("Less/Equal sign             "); addch(ACS_LEQUAL); printw("\n");
-        printw("Greater/Equal sign          "); addch(ACS_GEQUAL); printw("\n");
-        printw("Pi                          "); addch(ACS_PI); printw("\n");
-        printw("Not equal                   "); addch(ACS_NEQUAL); printw("\n");
-        printw("UK pound sign               "); addch(ACS_STERLING); printw("\n");
-
-        refresh();
-        getch();
-        endwin();
-
-	return 0;
+  return 0;
 }
